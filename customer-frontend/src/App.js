6533d00d08709c9adf0393a63ba2dad418987eb1
Lauri Anttila <lanttila98@gmail.com>
2023-03-17 22:40:26 +0200

    Initialized react app with tailwind and created crude form for player name and map name

---
+++ customer-frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import AnalyzeForm from './components/AnalyzeForm';
+
+function App() {
+  return (
+    <main className='w-[80%] mt-10 m-auto'>
+      <h1 className='text-3xl font-bold mb-10'>Analyze player</h1>
+      <AnalyzeForm />
+    </main>
+  );
+}
+
+export default App;
---
+++ customer-frontend/src/components/AnalyzeForm.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+class AnalyzeForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            player: '',
+            mapName: '',
+        }
+    }
+
+    handleChange = (event) => {
+        const property = event.target.getAttribute('name');
+        const value = event.target.value;
+        this.setState({ [property]: value });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state);
+    }
+
+    render() {
+        return <div className=''>
+            <h2>Input player name and map</h2>
+            <div className='my-2'>
+                <label className='inline-block w-36' htmlFor='player'>Player name</label>
+                <input onChange={this.handleChange} className='border-[1px] rounded' id='player' name='player' />
+            </div>
+            <div className='my-2'>
+                <label className='inline-block w-36' htmlFor='mapName'>Map name</label>
+                <input onChange={this.handleChange} className='border-[1px] rounded' id='mapName' name='mapName' />
+            </div>
+
+            <input onClick={this.handleSubmit} className='bg-green-300 text-white px-5 py-2 mt-5 cursor-pointer transition-all relative top-0 hover:-top-1' type='submit' value='Analyze' />
+        </div>;
+    }
+}
+
+export default AnalyzeForm;
