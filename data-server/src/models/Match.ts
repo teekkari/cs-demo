5954bedb78d7ec58bed8cace17a251dccac6844c
Lauri Anttila <lanttila98@gmail.com>
2023-03-18 22:31:28 +0200

    Fix match Schema for team_1 and team_2

--- data-server/src/models/Match.ts
+++ data-server/src/models/Match.ts
@@ -4,16 +4,18 @@ const matchSchema = new Schema({
     matchId: { type: String, required: true },          // matchId given by faceit
     startedAt: Date,                                    // DateTime of match beginning
     duration: Number,                                   // How long match took in secondss
-    team_1: [{                                          // List of all players in team 1 and require 5 players in team
-        type: Schema.Types.ObjectId,
+    team_1: {                                          // List of all players in team 1 and require 5 players in team
+        type: [Schema.Types.ObjectId],
         ref: 'Player',
-        required: (v: Object[]) => v.length == 5
-    }], 
-    team_2: [{                                          // List of all players in team 2 and require 5 players in team
-        type: Schema.Types.ObjectId,
+        required: true,
+        validate: (v: Object[]) => v.length == 5
+    }, 
+    team_2: {                                          // List of all players in team 2 and require 5 players in team
+        type: [Schema.Types.ObjectId],
         ref: 'Player',
-        required: (v: Object[]) => v.length == 5
-    }], 
+        required: true,
+        validate: (v: Object[]) => v.length == 5
+    }, 
     team_1_won: { type: Boolean, required: true },      // Store match winner with boolean indicating if team 1 won or lost
     mapName: { type: String, required: true},           // Which map was played e.g. 'de_mirage'
     averageElo: Number,                                 // Average ELO of all players
