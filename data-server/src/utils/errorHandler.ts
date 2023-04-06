b58d92e80494760dcd8b2c8f979c4cc775c7f389
Lauri Anttila <lanttila98@gmail.com>
2023-04-06 10:48:25 +0300

    Created dummy error handler to use in error cases. Proper handler need to be implemented later

---
+++ data-server/src/utils/errorHandler.ts
@@ -0,0 +1,10 @@
+
+// basic error handler just to get a single handler everywhere
+// => hash out the logic later for proper handler
+const handleError = (error: any): void => {
+    console.log(error);
+    return;
+};
+
+
+export default handleError;
